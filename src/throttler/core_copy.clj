52517712c6d97dcff332a4605aded08d83992a3b
Agata Sumowska <agata.sumowska@gmail.com>
2024-05-07 17:26:33 +0200

    simplified

--- src/throttler/core_copy.clj
+++ src/throttler/core_copy.clj
@@ -22,88 +22,24 @@
        (close! ~to)
        (>! ~to v#))))
 
-(defn- chan-throttler* [rate-ms bucket-size]
-  (let [sleep-time (round (max (/ rate-ms) min-sleep-time))
+(defn- throttle-chan [input-chan rate unit bucket-size]
+  (let [rate-ms (/ rate (unit->ms unit))
+        sleep-time (round (max (/ rate-ms) min-sleep-time))
         token-value (round (* sleep-time rate-ms))   ; how many messages to pipe per token
-        bucket (chan (dropping-buffer bucket-size))] ; we model the bucket with a buffered channel
-
-    ;; The bucket filler thread. Puts a token in the bucket every
-    ;; sleep-time seconds. If the bucket is full the token is dropped
-    ;; since the bucket channel uses a dropping buffer.
+        bucket (chan (dropping-buffer bucket-size))] ; we model the bucket with a buffered channel 
     (go
       (while (>! bucket :token)
         (<! (timeout (int sleep-time)))))
-
-    ;; The piping thread. Takes a token from the bucket (blocking until
-    ;; one is ready if the bucket is empty), and forwards token-value
-    ;; messages from the source channel to the output channel.
-
-    ;; For high frequencies, we leave sleep-time fixed to
-    ;; min-sleep-time, and we increase token-value, the number of
-    ;; messages to pipe per token. For low frequencies, the token-value
-    ;; is 1 and we adjust sleep-time to obtain the desired rate.
-
-    (fn [c]
-      (let [c' (chan)] ; the throttled chan
-        (go
-          ;; (while (<! bucket) ; block for a token
-          (while true
-            (let [[v _]  (alts! [bucket] :default false)]
-              (if v
-                (dotimes [_ token-value]
-                  (when-not (pipe c c')
-                    (close! bucket)))
-                (>! c' :rejected)))))
-        c'))))
-
-(defn chan-throttler
-  "Returns a function that will take an input channel and return an
-   output channel with the desired rate. Optionally acceps a bucket size
-   for bursty channels.
-
-   If the throttling function returned here is used on more than one
-   channel, they will all share the same token-bucket. This means their
-   overall output rate combined will be equal to the provided rate. In
-   other words, they will all share the alloted bandwith using
-   statistical multiplexing.
-
-   See fn-throttler for an example that can trivially be extrapolated to
-   chan-throttler."
-
-  ([rate unit]
-   (chan-throttler rate unit 1))
-  ([rate unit bucket-size]
-   (let [rate-ms (/ rate (unit->ms unit))]
-     (chan-throttler* rate-ms bucket-size))))
-
-(defn throttle-chan
-  "Takes a write channel, a goal rate and a unit and returns a read
-      channel. Messages written to the input channel can be read from
-      the throttled output channel at a rate that will be at most the
-      provided goal rate.
-
-      Optionally takes a bucket size, which will correspond to the
-      maximum number of burst messages.
-
-      As an example, the channel produced by calling:
-
-      (throttle-chan (chan) 1 :second 9)
-
-      Will transmit 1 message/second on average but can transmit up to
-      10 messages on a single second (9 burst messages + 1
-      message/second).
-
-      Note that after the burst messages have been consumed they have to
-      be refilled in a quiescent period at the provided rate, so the
-      overall goal rate is not affected in the long term.
-
-      The throttled channel will be closed when the input channel
-      closes."
-
-  
-
-  ([c rate unit bucket-size]
-   ((chan-throttler rate unit bucket-size) c)))
+    (let [output-chan (chan)] ; the throttled chan
+      (go
+        (while true
+          (let [[v _]  (alts! [bucket] :default false)]
+            (if v
+              (dotimes [_ token-value]
+                (when-not (pipe input-chan output-chan)
+                  (close! bucket)))
+              (>! output-chan :rejected)))))
+      output-chan)))
 
 
 
@@ -145,8 +81,6 @@
   ;;     (>!! in :eval-request)
   ;;     (<!! out)
   ;;     (apply + args))
-  (def ex (experiment))
-  (ex 123)
   
   (def tomato-echo (chan 2))
   (>!! tomato-echo :sth)
